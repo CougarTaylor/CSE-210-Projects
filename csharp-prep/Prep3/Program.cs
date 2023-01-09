8f0ed2cd263897c07f36a60b776737dc9fe833cd
Cougar Taylor <ghostcat@byui.edui>
2023-01-09 15:09:01 -0700

    Challenge complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,34 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 101);
-        // string magicNumber = number.ToString();
-
-        Console.WriteLine("\nWelcome to the Magic Number Game! \nWe've picked a magic number form 1 to 10.");
+        Console.WriteLine("\nWelcome to the Magic Number Game! \nWe've picked a magic number form 1 to 100.");
 
         string input;
         int guess;
+        int numOfGuesses = 0;
+        string playMore = null;
         do
         {
-            Console.Write("What is your guess? ");
-            input = Console.ReadLine();
-            guess = int.Parse(input);
-            if (guess < number)
+            
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+            do
             {
-                Console.WriteLine("Too Low!");
-            }
-            else if (guess > number)
-            {
-                Console.WriteLine("Too High!");
-            }
-            else
-            {
-                break;
-            }
-        } while (guess != number);
+                Console.Write("What is your guess? ");
+                input = Console.ReadLine();
+                guess = int.Parse(input);
+                if (guess < number)
+                {
+                    Console.WriteLine("Too Low!");
+                    numOfGuesses += 1;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too High!");
+                    numOfGuesses += 1;
+                }
+                else
+                {
+                    break;
+                }
+            } while (guess != number);
 
-        Console.Write("You Guessed it!");
+            Console.WriteLine($"You Guessed it!  You took {numOfGuesses} tries.");
+            Console.Write("Would you like to play again? ");
+            playMore = Console.ReadLine();
+            if (playMore == "yes") 
+            {
+                Console.WriteLine();
+                Console.WriteLine("Great! A new number has been chosen.");
+                numOfGuesses = 0;
+            }
+        } while (playMore == "yes");
     }
 }
