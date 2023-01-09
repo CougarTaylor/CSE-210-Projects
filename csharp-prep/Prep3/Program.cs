c6110e8fd59b79efc467118d1bc9d0f67305c130
Cougar Taylor <ghostcat@byui.edui>
2023-01-09 14:30:58 -0700

    Core Requirements met

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        // string magicNumber = number.ToString();
+
+        Console.WriteLine("\nWelcome to the Magic Number Game! \nWe've picked a magic number form 1 to 10.");
+
+        string input;
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+            if (guess < number)
+            {
+                Console.WriteLine("Too Low!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else
+            {
+                break;
+            }
+        } while (guess != number);
+
+        Console.Write("You Guessed it!");
     }
 }
