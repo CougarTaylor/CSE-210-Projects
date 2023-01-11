3faa409dae84bd0ab36e2ed4fee8919d75ed280a
Cougar Taylor <ghostcat@byui.edui>
2023-01-10 20:41:09 -0700

    Prep5 req met

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName =  PromptUserName();
+        int userNum = PromptUserNumber();
+        int squaredNum = SquareNumber(userNum);
+        DisplayResult(userName, squaredNum);
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string numberString = Console.ReadLine();
+            int number = int.Parse(numberString);
+            return number;
+        }
+
+        static int SquareNumber(int originalNum)
+        {
+            int newNum = originalNum * originalNum;
+            return newNum;
+        }
+        
+        static void DisplayResult(string name, int num)
+        {
+            Console.Write($"{name}, the square of your number is {num}");
+        }
+
     }
 }
