12a8e1ace6f761d8f11bb11efce61eae09a77752
Cougar Taylor <ghostcat@byui.edui>
2023-02-22 22:12:19 -0700

    week 8 practice complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment task = new Assignment("Jack Daniels", "Liqour Samples");
+        Console.WriteLine(task.GetSummary());
+
+        MathAssignment task2 = new MathAssignment("Hammy Commings", "Stimulations", "6.9", "4-20");
+        Console.WriteLine(task2.GetSummary());
+        Console.WriteLine(task2.GetHomeWorkList());
+
+        WritingAssignment task3 = new WritingAssignment("Jason Borne", "Withering", "European History");
+        Console.WriteLine(task3.GetWritingInformation());
     }
 }
